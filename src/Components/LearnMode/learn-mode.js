24d5dfd7458de093cf637bd7311b580fcc882c84
rahul narakula <narakularahul@gmail.com>
2019-04-12 15:49:26 +0530

    Bootstrap styles added

--- src/Components/LearnMode/learn-mode.js
+++ src/Components/LearnMode/learn-mode.js
@@ -3,84 +3,151 @@ import React, { Component } from 'react';
 export class LearnMode extends Component{
     render() {
         return (
-            <div className="container">
-                <div className="row my-3">
-                    <div className="main-word text-center mx-auto">
-                        <h3>tortuous</h3>
-                        <h5>TAWR-choo-uhs</h5>
-                    </div>
-                </div>
+            <div className="container">                
+                <WordHeader />
+                <WordContent />
+                <WordIngredient />
+            </div>
+        );
+    }
+}
 
-                <div className="row">
-                    <div className="row context-heading">Context</div>
-                    <div className="row context-body">
-                        <div className="context-statement">
-                            Nina’s long and tortuous journey was filled with twists and turns along the mountain road. Although Nina had noticed that her chosen route would have many bends, she had had no idea that she was in for such a tortuous or complex ride. When she had finally navigated through that winding, tortuous, and roundabout road, she felt like she too was all wound up.
-                        </div>
-                        <div className="context-question">
-                            <h3>When is something considered tortuous?</h3>
-                        </div>
-                        <div className="context-options">
-                            <ul id="choice-section">
-                                <li className="choice answer ">
-                                <span className="result">a.</span>
-                                When it is complicated with many turns and bends.
-                                </li>
-                                <li className="choice ">
-                                <span className="result">b.</span>
-                                When it takes all day long to complete.
-                                </li>
-                                <li className="choice ">
-                                <span className="result">c.</span>
-                                When it almost makes you sick.
-                                </li>
-                            </ul>
-                        </div>
-                        <div id="definition">
-                            <a className="show-definition">Definition</a>
-                            <div className="def-bubble" >
-                                Something that is <em>tortuous</em>, like a piece of writing, is long and complicated with many twists and turns of direction; a <em>tortuous</em> argument can be deceitful because it twists or turns the truth.
-                            </div>
-                        </div>
-                    </div>
+class WordContent extends Component{
+    constructor(props){
+        super(props);
+    }
 
-                </div>
-
-                <div className="row">
-                <div class="module-medium panel " id="word-structure">
-<h3 class="title">
-Word Ingredients
-</h3>
-<div class="content">
-<table>
-<tbody><tr>
-<td>
-<a class="wi-btn partform" data-tree-id="tort_twisted" data-tree-url="//cdn1.membean.com/public/data/treexml" href="#">
-<span class=""></span>
-tort
-</a>
-</td>
-<td></td>
-<td class="meaning">twisted, wound, wrapped</td>
-</tr>
-<tr>
-<td>
-<a class="wi-btn partform" data-tree-id="uous_nature" data-tree-url="//cdn1.membean.com/public/data/treexml" href="#">
-<span class=""></span>
--uous
-</a>
-</td>
-<td></td>
-<td class="meaning">of the nature of</td>
-</tr>
-</tbody></table>
-<p>Something <em>tortuous</em>, like a mountain road or difficult language, is “twisted, wound, or wrapped” up so as to make it complex to navigate.</p>
-
-
-</div>
-</div>
+    render(){           
+        const content = "Nina’s long and tortuous journey was filled with twists and turns along the mountain road. Although Nina had noticed that her chosen route would have many bends, she had had no idea that she was in for such a tortuous or complex ride. When she had finally navigated through that winding, tortuous, and roundabout road, she felt like she too was all wound up";
+        const questionContent = {
+                                    question: "When is something considered tortuous?",
+                                    answers: [
+                                        "When it is complicated with many turns and bends.",
+                                        "When it takes all day long to complete.",
+                                        "When it almost makes you sick."
+                                    ],
+                                    correct_answer:1
+                                };                    
+        return (
+            <div className="card">
+                <div className="context-heading card-header">Context</div>
+                <div className="context-body card-body">
+                    <Statement content={content}/>
+                    <Question content={questionContent}/>                
+                    <Definition />
                 </div>
             </div>
         );
     }
+}
+
+function Statement(props){
+    return (
+        <div className="context-statement">
+            {props.content}
+        </div>
+    );
+}
+
+class Question extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            content:props.content,
+            is_question_completed: false
+                    }
+    }
+    handleClick(index){
+        if(!this.state.is_question_completed){
+            if(index==this.state.content.correct_answer){
+                console.log("correct");
+                this.setState({is_question_completed:true});
+            } else{
+                console.log("wrong");
+            }
+        } else{
+            return false;
+        }
+    }
+    render(){
+        const listAnswers = this.state.content.answers.map((answer,index)=>
+            <li className="choice answer border my-3 rounded p-2" key={index+1} onClick={()=>this.handleClick(index+1)}>
+                <span className="result mr-2">{index+1}.</span>
+                {answer}
+            </li>
+        );
+
+        return (
+            <div className="question-answer">
+                <div className="context-question mt-4">
+                    <h3 className='ml-3'>{this.state.content.question}</h3>
+                </div>
+                <div className="context-options">
+                    <ul id="choice-section" className='pr-5'>
+                    {listAnswers}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
+}
+
+function WordHeader(){
+    return (
+        <div className="row my-3">
+                    <div className="main-word text-center mx-auto">
+                        <h1 className='text-uppercase'>tortuous</h1>
+                        <h5>TAWR-choo-uhs</h5>
+                    </div>
+                </div>
+    );
+}
+
+function WordIngredient(){
+    return (
+        
+        <div className="card my-5">
+            <div className="module-medium panel " id="word-structure">
+                <h3 className="title card-header">Word Ingredients</h3>
+                <div className="content card-body">
+                    <table>
+                        <tbody>
+                            <tr>
+                                <td>
+                                    <a className="wi-btn partform" data-tree-id="tort_twisted" data-tree-url="//cdn1.membean.com/public/data/treexml" href="#">
+                                        <span className=""></span>
+                                        tort
+                                    </a>
+                                </td>
+                                <td></td>
+                                <td className="meaning">twisted, wound, wrapped</td>
+                            </tr>
+                            <tr>
+                                <td>
+                                    <a className="wi-btn partform" data-tree-id="uous_nature" data-tree-url="//cdn1.membean.com/public/data/treexml" href="#">
+                                        <span className=""></span>
+                                        -uous
+                                    </a>
+                                </td>
+                                <td></td>
+                                <td className="meaning">of the nature of</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <p>Something <em>tortuous</em>, like a mountain road or difficult language, is “twisted, wound, or wrapped” up so as to make it complex to navigate.</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Definition(){
+    return (
+        <div id="definition">
+            <button className="show-definition btn btn-primary my-2">Definition</button>
+            <div className="def-bubble bg-info text-white p-3">
+                Something that is <em>tortuous</em>, like a piece of writing, is long and complicated with many twists and turns of direction; a <em>tortuous</em> argument can be deceitful because it twists or turns the truth.
+            </div>
+        </div>
+    );
 }
