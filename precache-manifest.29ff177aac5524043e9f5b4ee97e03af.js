c8a0ea3810dd5a974de92f946012cbe7cfad6a15
rahulnarakula <narakularahul@gmail.com>
2020-10-31 19:05:42 +0530

    Updates

---
+++ precache-manifest.29ff177aac5524043e9f5b4ee97e03af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "64dd1ee12593f6de042a",
+    "url": "/vocabularyBuilder/static/js/runtime~main.1340b983.js"
+  },
+  {
+    "revision": "d800ac9a993fd4d0c485",
+    "url": "/vocabularyBuilder/static/js/main.69e5c8af.chunk.js"
+  },
+  {
+    "revision": "f22dd90d4d4da5d3a5c7",
+    "url": "/vocabularyBuilder/static/js/2.12f5ff5a.chunk.js"
+  },
+  {
+    "revision": "d800ac9a993fd4d0c485",
+    "url": "/vocabularyBuilder/static/css/main.45433e17.chunk.css"
+  },
+  {
+    "revision": "f22dd90d4d4da5d3a5c7",
+    "url": "/vocabularyBuilder/static/css/2.984f5e3c.chunk.css"
+  },
+  {
+    "revision": "427ff205665160bb7e9a0111e25a4536",
+    "url": "/vocabularyBuilder/index.html"
+  }
+];
